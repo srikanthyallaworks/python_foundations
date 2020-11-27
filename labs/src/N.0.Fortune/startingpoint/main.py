ebdce05313a31917c76585ce466c963ab2a44e95
bathcat <you@example.com>
2020-11-27 13:19:54 -0700

    .

---
+++ labs/src/N.0.Fortune/startingpoint/main.py
@@ -0,0 +1,17 @@
+"""
+
+
+
+"""
+
+def toText(n):
+    return str(n)
+
+
+def proveWorth():
+    for i in [n + 1 for n in range(99)]:
+        print(toText(i))
+
+
+if __name__ == "__main__":
+    proveWorth()
