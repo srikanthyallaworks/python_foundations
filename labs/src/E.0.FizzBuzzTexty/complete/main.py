561b213ecac0042174aa0987058d6d58b16bab7a
bathcat <you@example.com>
2020-11-28 09:44:29 -0700

    .

--- labs/src/E.0.FizzBuzzTexty/complete/main.py
+++ labs/src/E.0.FizzBuzzTexty/complete/main.py
@@ -1,8 +1,31 @@
 """
 
+# Lab: Fizz Buzz
 
+## Summary: 
+
+Surprisingly, lots of professional programmers can't really 
+code[1]. FizzBuzz is a classic interview question for sussing-out 
+the non-skilled. 
+                  
+Here it is:
+    Write a program that prints the numbers from 1 to 100. 
+    But--
+      * For multiples of 3 print “Fizz” instead of the number 
+      * For the multiples of 5 print “Buzz”. 
+      * For numbers which are multiples of both 3 and 5 print “FizzBuzz”.
+
+## Requirements
+* Implement toText method.
+* Write the correct output--described above-- to the console.
+* Write specs to document and test.
+
+Notes:
+1. https://blog.codinghorror.com/why-cant-programmers-program/
 
 """
+
+
 
 def toText(n):
     divisibleBy3 = n % 3 == 0
@@ -16,12 +39,12 @@ def toText(n):
     return str(n)
 
 
-def proveWorth():
-    for i in [n + 1 for n in range(99)]:
+def main():
+    for i in range(1,101):
         print(toText(i))
 
 
 if __name__ == "__main__":
-    proveWorth()
+    main()
 
 
--- labs/src/E.0.FizzBuzzTexty/startingpoint/main.py
+++ labs/src/E.0.FizzBuzzTexty/startingpoint/main.py
@@ -1,6 +1,27 @@
 """
 
+# Lab: Fizz Buzz
 
+## Summary: 
+
+Surprisingly, lots of professional programmers can't really 
+code[1]. FizzBuzz is a classic interview question for sussing-out 
+the non-skilled. 
+                  
+Here it is:
+    Write a program that prints the numbers from 1 to 100. 
+    But--
+      * For multiples of 3 print “Fizz” instead of the number 
+      * For the multiples of 5 print “Buzz”. 
+      * For numbers which are multiples of both 3 and 5 print “FizzBuzz”.
+
+## Requirements
+* Implement toText method.
+* Write the correct output--described above-- to the console.
+* Write specs to document and test.
+
+Notes:
+1. https://blog.codinghorror.com/why-cant-programmers-program/
 
 """
 
@@ -9,9 +30,8 @@ def toText(n):
 
 
 def main():
-    for i in [n + 1 for n in range(99)]:
+    for i in [1,2,3,4,5,6,8,9,10]:
         print(toText(i))
-
 
 if __name__ == "__main__":
     main()
