38803e51329df3224cfdb5ed83f574884e3a940a
bathcat <you@example.com>
2020-11-28 10:44:21 -0700

    .

--- labs/src/N.0.Fortune/complete/main.py
+++ labs/src/N.0.Fortune/complete/main.py
@@ -19,19 +19,17 @@ this_directory = os.path.dirname(__file__)
 parent_directory = os.path.dirname(this_directory)
 data_file = os.path.join(parent_directory, '_data/fortunes.txt')
 
-fortunes = [
-    'You will obey or molten silver will be poured into your ears.',
-    'You will outgrow your usefulness.',
-    'You will overcome the attacks of jealous associates.',
-    'You will pass away very quickly.',
-    'You will pay for your sins.  If you have already paid, please disregard this message.',
-    'You will pioneer the first Martian colony.']
-
-def get_fortune():
-  return fortunes[0]
+def get_fortunes():
+  fortunes = []
+  reader = open(data_file)
+  for line in reader:
+    fortunes.append(line)
+  reader.close()
+  return fortunes
 
 def main():
-  message = get_fortune()
+  fortunes=get_fortunes()
+  message = random.choice(fortunes)
   print('\nYour fortune is:')
   print(f'   {message}\n\n')
 
