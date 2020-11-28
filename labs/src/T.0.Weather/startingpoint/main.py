12a74ff536bb6bd2490525fd4250caeaaaf2827b
bathcat <you@example.com>
2020-11-28 13:15:34 -0700

    .

--- labs/src/T.0.Weather/startingpoint/main.py
+++ labs/src/T.0.Weather/startingpoint/main.py
@@ -22,13 +22,21 @@ Here it is:
 Notes:
 1. https://blog.codinghorror.com/why-cant-programmers-program/
 
-"""
+Icons here: 
+  - https://www.flaticon.com/free-icon/sun_2698194?term=sunny&page=1&position=34
 
+"""
+import os
 import unicornhathd
 import math
 import requests
 import time
 from PIL import Image
+
+this_directory = os.path.dirname(__file__)
+parent_directory = os.path.dirname(this_directory)
+img_file = os.path.join(parent_directory, '_data/snow.png')
+img = Image.open(img_file)
 
 
 def fetch(uri):
@@ -63,11 +71,40 @@ def get_forecast(office,x,y):
   json = fetch(url)["properties"]
   return {}
 
+width=16
+height=16
+
+unicornhathd.set_rotation(-90)
+
+def draw():
+  try:
+      while True:
+          for o_x in range(int(img.size[0] / width)):
+              for o_y in range(int(img.size[1] / height)):
+
+                  valid = False
+                  for x in range(width):
+                      for y in range(height):
+                          pixel = img.getpixel(((o_x * width) + y, (o_y * height) + x))
+                          r, g, b = int(pixel[0]), int(pixel[1]), int(pixel[2])
+                          if r or g or b:
+                              valid = True
+                          unicornhathd.set_pixel(x, y, r, g, b)
+
+                  if valid:
+                      unicornhathd.show()
+                      time.sleep(0.5)
+
+  except KeyboardInterrupt:
+      unicornhathd.off()
+
+
 def main():
-  print('\n\n\n===========================\n')
+  draw()
+  #print('\n\n\n===========================\n')
   #print(get_geo_location())
-  print(get_grid_location(39.7628,-105.0263))
-  print('\n\n\n\n')
+  #print(get_grid_location(39.7628,-105.0263))
+  #print('\n\n\n\n')
 
 if __name__ == "__main__":
     main()
