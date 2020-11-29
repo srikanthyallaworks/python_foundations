1ee7f4934f9d2f5f435abe183f47f447386a34c5
bathcat <you@example.com>
2020-11-29 13:05:01 -0700

    .

--- labs/src/G.0.ArrayGolf/complete/main.py
+++ labs/src/G.0.ArrayGolf/complete/main.py
@@ -1,7 +1,15 @@
-"""
+from array_tools_tests import run_tests
+from array import array
+
+def main():
+    eve0 = {'id':11,'givenName':'eve'}
+    eve1 = {'id':11,'givenName':'eve'}
+    print(eve0==eve1)
+#   print('\n\n\nTODO: Get these tests passing.')
+#   run_tests()
+#   print('\n')
 
 
+if __name__ == '__main__':
+  main()
 
-"""
-
-print("lorem ipsum")
