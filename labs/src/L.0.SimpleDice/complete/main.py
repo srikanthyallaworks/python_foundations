459e84a4029e78b9d01e0ccf765499d892529ca4
bathcat <you@example.com>
2020-11-29 15:16:52 -0700

    .

--- labs/src/L.0.SimpleDice/complete/main.py
+++ labs/src/L.0.SimpleDice/complete/main.py
@@ -4,4 +4,48 @@
 
 """
 
-print("lorem ipsum")
+import random
+
+class Die():
+
+  def __init__(self,sideCount):
+    self.sideCount=sideCount
+    self._value = None
+
+  def get_value(self):
+    return self._value; 
+
+  def roll(self):
+    self._value = random.randrange(1,self.sideCount+1)
+    return self._value
+
+
+class Cup():
+
+  def __init__(self,dice):
+    self._dice = dice
+
+  def roll(self):
+    total=0
+    for die in self._dice:
+      total+=die.roll()
+    return total
+
+  def get_value(self):
+    total=0
+    for die in self._dice:
+      total+=die.get_value()
+    return total
+
+
+def main():
+  cup = Cup([Die(6),Die(6),Die(6)])
+  while True:
+    result = cup.roll()
+    print(f'Rolled a {result}')
+    if result==18:
+      print('You won!')
+      return  
+
+if __name__ == "__main__":
+    main()
