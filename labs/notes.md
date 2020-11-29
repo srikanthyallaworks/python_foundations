7357fcc0b953087e1fe5af05b7c95bd3612993fa
bathcat <you@example.com>
2020-11-29 11:26:54 -0700

    .

--- labs/notes.md
+++ labs/notes.md
@@ -8,14 +8,40 @@
 
 ## Installing
 
+
+
+
 # https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
 
+Install venv like this:
+sudo apt-get install python3-pip python3-venv
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user virtualenv
 
-git clone http://loremipsum/python_foundations
+
+
+
+
+git clone https://github.com/bathcat/python_foundations
 cd python_foundations/labs
+
+
+
+
+export PATH="$(python3 -m site --user-base)/bin:${PATH}"
 python3 -m venv .venv
 source ./.venv/bin/activate
 pip install -r requirements.txt
+
+
+  error: invalid command 'bdist_wheel'
+  ----------------------------------------
+  ERROR: Failed building wheel for wrapt
+
+
+
+  WARNING: The scripts pip, pip3 and pip3.8 are installed in '/home/bathcat/.local/bin' which is not on PATH.
+  Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
 
 
 
--- labs/src/L.1.VirtuousDice/complete/main.py
+++ labs/src/L.1.VirtuousDice/complete/main.py
@@ -35,4 +35,134 @@ Rules:
 
 """
 
-print("lorem ipsum")
+import random
+from enum import Enum
+
+class Die:
+  def __init__(self,sideCount=6):
+    self.sideCount = sideCount
+    self.value=None
+  
+  def roll(self):
+    self.value = random.randrange(1,self.sideCount+1)
+  
+
+class Cup:
+  def __init__(self,dice):
+    self.dice = dice
+
+  def roll(self):
+    pass #TODO: .dice.forEach(d=>d.roll());
+  
+  def value(self):
+    sum = 0
+    for die in self.dice:
+      sum += die.value
+    return sum
+
+
+
+class Outcome(Enum):
+  Undecided=0
+  Win=1
+  Loss=-2
+
+
+
+
+
+class Game:
+  def __init__(self,playerName):
+    self.playerName=playerName
+    self.outcome = Outcome.Undecided
+    self.cup= Cup([Die(), Die()])
+  
+
+  def shoot(self):
+    self.cup.roll()
+    #console.log(`${this.playerName} rolls a ${this.cup.value}`);
+    return self.cup.value
+  
+
+#   _won(){
+#     this.outcome= Outcome.Win;
+#     console.log('Game Over: Won!');
+#     this._over();
+#   }
+
+#   _lost(){
+#     this.outcome= Outcome.Loss;
+#     console.log('Game Over: Lost');
+#     this._over();
+#   }
+
+  def play(self):
+    firstRoll = self.shoot()
+
+    if firstRoll in [2,3,12]:
+      raise AssertionError("you lost")
+
+    if firstRoll in [7,11]:
+      raise AssertionError("you won!")
+    
+    while(self.outcome == Outcome.Undecided):
+      result = self.shoot()
+
+      if result==7:
+        raise AssertionError("you lost")
+
+      if result==firstRoll:
+        raise AssertionError("you won!")
+        
+
+
+
+
+
+
+class Table:
+  def __init__(self,players):
+    self.players = players
+    self.gameNumber=0  
+
+  def getCurrentPlayer(self):
+    return self.players[self.gameNumber%self.players.length]
+  
+
+  def playGame(self):
+    self.gameNumber += 1
+    game = Game(self.currentPlayer)
+    game.play();
+    console.log();
+  
+
+
+
+
+
+
+def main():
+  d0 = Die()
+  d0.roll()
+  print(d0.value)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+# const table = new Table(["Dave", "Erin", "Heather","Varoon"]);
+
+# table.playGame();
+
+# console.log();
+# console.log('Lab complete!');
+# console.log();
