378cf2aca608a9febe61c04b727fbbdb27806979
bathcat <you@example.com>
2020-11-30 12:06:05 -0700

    .

--- labs/src/T.0.DoNotDisturb/complete/main.py
+++ labs/src/T.0.DoNotDisturb/complete/main.py
@@ -5,33 +5,39 @@
 
 
 """
-import asyncio
+
 import os
 import sys
 import time
 import subprocess
 
+
 this_directory = os.path.dirname(__file__)
-server_py = os.path.join(this_directory, 'server.py')
-indicator_py = os.path.join(this_directory, 'indicator.py')
+
+
+def run_script(script):
+  return subprocess.Popen([sys.executable, script], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+
+def run_indicator():
+  indicator_py = os.path.join(this_directory, 'indicator.py')
+  return run_script(indicator_py)
+
+
+def run_server():
+  server_py = os.path.join(this_directory, 'server.py')
+  return run_script(server_py)
+
 
 def main():
-  print(f'running: {sys.executable} {server_py}')
-  
-  process_server = subprocess.Popen([sys.executable, server_py], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  process_indicator = subprocess.Popen([sys.executable, indicator_py], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  with run_indicator() as process_indicator, run_server() as process_server:
+    while True:
+      print(process_server.stdout.readlines(10))
+      print(process_indicator.stdout.readlines(10))
+      time.sleep(2)
+  print('Exiting')
 
-  for i in range(30):
-    print(process_server.stdout.readlines(10))
-    print(process_indicator.stdout.readlines(10))
-    time.sleep(2)
-  process_indicator.kill()
-  process_server.kill()
-  print('done')
 
 if __name__ == '__main__':
   main()
-
-
-
 
