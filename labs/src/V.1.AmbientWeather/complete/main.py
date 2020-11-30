a331ad132dd82b22519566197f4e2ae60fd85506
bathcat <you@example.com>
2020-11-30 12:49:17 -0700

    .

--- labs/src/V.1.AmbientWeather/complete/main.py
+++ labs/src/V.1.AmbientWeather/complete/main.py
@@ -39,63 +39,77 @@ from enum import Enum
 from forecast import get_local_forecast
 
 
+rows=16
+columns=16
+poll_frequency=90
 
 class Forecast(Enum):
+  Unknown=0
   Sun=1
   Snow=2
-  Thunder=3
-  Wind=4
-  Storm=5
-
-class TimeOfDay(Enum):
-  Day=100
-  Night=101
+  Rain=3
 
 icons = {
   Forecast.Snow:'snow.png',
   Forecast.Sun:'sun.png',
-  Forecast.Thunder: 'thunder.png',
-  Forecast.Wind:'wind.png',
-  Forecast.Storm:'storm.png',
-  TimeOfDay.Night:'night.png',
+  Forecast.Rain: 'rain.png',
+  Forecast.Unknown:'unknown.png',
 }
 
-def getImage(forecast):
+def to_image(forecast):
   this_directory = os.path.dirname(__file__)
   parent_directory = os.path.dirname(this_directory)
   img_file = os.path.join(parent_directory, f'_assets/{icons[forecast]}')
   return Image.open(img_file)
 
 
+def to_forecast(short_forecast):
+  lowered = short_forecast.lower()
+  if "sun" in lowered:
+    return Forecast.Sun
+  if "rain" in lowered:
+    return Forecast.Rain
+  if "snow" in lowered:
+    return Forecast.Snow
+  return Forecast.Unknown
+
+
 def get_short_forecast():
   return get_local_forecast()["properties"]["periods"][0]["shortForecast"]
 
+def get_image():
+  short_forecast = get_short_forecast()
+  forecast = to_forecast(short_forecast)
+  return to_image(forecast)
 
-def draw():
-  width=16
-  height=16
+
+
+def update():
+  img = get_image()
+
+  for row in range(rows):
+    for column in range(columns):
+      pixel = img.getpixel(( column, row))
+      r, g, b, o = int(pixel[0]), int(pixel[1]), int(pixel[2]),float(pixel[3])
+      if(o>0):
+        unicornhathd.set_pixel(row, column, r, g, b)
+
+  unicornhathd.show()
+
+
+def run():
   unicornhathd.set_rotation(-90)
-  img = getImage(Forecast.Sun)
-
-  try:
-      while True:
-        for x in range(width):
-          for y in range(height):
-                pixel = img.getpixel(( y, x))
-                r, g, b, o = int(pixel[0]), int(pixel[1]), int(pixel[2]),float(pixel[3])
-                if(o>0):
-                  unicornhathd.set_pixel(x, y, r, g, b)
-
-        unicornhathd.show()
-        time.sleep(0.5)
-
-  except KeyboardInterrupt:
-      unicornhathd.off()
+  update()
+  while True:
+    update()
+    time.sleep(poll_frequency)
 
 
 def main():
-  draw()
-
+  try:
+    run()
+  except KeyboardInterrupt:
+    unicornhathd.off()
 
   
 if __name__ == "__main__":
