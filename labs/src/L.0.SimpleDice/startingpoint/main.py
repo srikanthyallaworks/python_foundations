19de98dbf76c54f114e54ea08f5e41ee01991611
bathcat <you@example.com>
2020-12-05 15:44:08 -0700

    .

---
+++ labs/src/L.0.SimpleDice/startingpoint/main.py
@@ -0,0 +1,21 @@
+import random
+
+
+class Die:
+  """Represents a physical, rollable, multi-sided die
+  """
+  pass
+
+
+class Cup:
+  """Represents a cup for holding and rolling multiple dice
+  """
+  pass
+
+
+def main():
+  print('TODO: Keep rolling 3 dice until you get to 18')
+
+
+if __name__ == "__main__":
+    main()
