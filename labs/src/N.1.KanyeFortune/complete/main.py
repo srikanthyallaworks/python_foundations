3b7923928edf2b9862fe5fa92d121ae5e62a356d
bathcat <you@example.com>
2020-12-05 14:52:48 -0700

    .

--- labs/src/N.1.KanyeFortune/complete/main.py
+++ labs/src/N.1.KanyeFortune/complete/main.py
@@ -1,18 +1,3 @@
-"""
-
-https://realpython.com/api-integration-in-python/
-https://en.wikipedia.org/wiki/Fortune_(Unix)
-
-
-## Requirements
-1. 
-
-## Stretch Goals
-1. Read-in the actual format.
-2. Support command-line options
-
-"""
-
 import requests
 
 def get_kanye_fortune()->str:
