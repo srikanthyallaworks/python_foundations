bbf2183da46ed7c130e273d681f0c7e3426effb5
bathcat <you@example.com>
2020-12-05 11:58:08 -0700

    .

--- labs/src/G.1.DictionaryGolf/fancy/main.py
+++ labs/src/G.1.DictionaryGolf/fancy/main.py
@@ -9,12 +9,24 @@ from collections import defaultdict
 
 
 def get_frequencies_by(field):
+  """Builds a dictionary counting repeat occurances of the specified field
+
+  Args:
+      field to count by. e.g. 'surname'
+
+  """
   counts = defaultdict(int)
   for student in roster:
     counts[student[field]] +=1
   return counts
 
+
 def sort_by_frequency(frequency_dict):
+  """Sorts the specified frequency dictionary
+
+  Returns:
+      Sorted list
+  """
   frequency_list = []
   for key, value in frequency_dict.items():
     frequency_list.append({
@@ -23,11 +35,13 @@ def sort_by_frequency(frequency_dict):
     })
   return sorted(frequency_list,key=lambda r:r['count'],reverse=True)
 
+
 def report_top_5_frequencies(field):
   counts = get_frequencies_by(field)
   sorted = sort_by_frequency(counts)
   for item in sorted[:5]:
     print(f'\t{item["key"]}: {item["count"]}')   
+
 
 def run_surname_report():
   print(f'\nHere are the top 5 surnames:')  
@@ -37,10 +51,14 @@ def run_givenName_report():
   print(f'\nHere are the top 5 given names:')  
   report_top_5_frequencies('givenName')
 
+
 def run_class_size_report():
-  print(f'\nHere are the students in each grade:')  
-  for key,value in get_frequencies_by('grade').items():
-    print(f'\t{key}: {value}')   
+  print(f'\nHere are the students in each grade:')
+  frequencies = get_frequencies_by('grade')
+  sorted_by_grade = sorted(frequencies.items(),key=lambda t:t[0])
+  for grade,count in sorted_by_grade:
+    print(f'\t{grade}: {count}')   
+
 
 
 def main():
