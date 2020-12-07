e762f35497697f94d2b393d3027f396126c8fc61
bathcat <you@example.com>
2020-12-07 16:44:11 -0700

    .

--- slides/start.py
+++ slides/start.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
 import sys
-
-if sys.version_info[0] < 3:
-    raise Exception("Must be using Python 3")
-
 import http.server
+import socketserver
 import webbrowser
 
 port = 8000
+directory = "www"
 url = f'http://localhost:{port}'
 
-def main(server_class=http.server.HTTPServer, handler_class=http.server.SimpleHTTPRequestHandler):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=directory, **kwargs)
+
+
+def main():
     print(f'Serving @{url}')
     webbrowser.open_new(url)
-    httpd.serve_forever()
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+    	httpd.serve_forever()
 
+    	
 if __name__ == "__main__":
     main()
