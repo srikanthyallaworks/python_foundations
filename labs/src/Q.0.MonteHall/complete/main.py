8abc9dee33ba4d2c268f3b0a9aad1e9ff24fe075
bathcat <you@example.com>
2020-12-07 12:13:17 -0700

    .

--- labs/src/Q.0.MonteHall/complete/main.py
+++ labs/src/Q.0.MonteHall/complete/main.py
@@ -9,8 +9,6 @@ def first(predicate, iterable):
     if predicate(item):
       return item
 
-
-
 @dataclass(frozen=True)
 class Prize:
   name:str 
@@ -47,83 +45,44 @@ class Doors:
     return [Door(prize) for prize in prizes]
 
 
-
-
-@dataclass
-class Host:
-  name:str
-  def reveal_goat(self,doors)->Door:
-    def is_candidate(door:Door):
-      return door.state!=DoorState.InitialSelection and door.prize.value==0
-    return first(is_candidate,doors)
-
-
-
-class Contestant:
-  def __init__(self,name):
-    self.name=name
-
-  def make_initial_selection(self,doors):
-    return random.choice(doors)
-
+def choose_stick(doors):
   def is_stick_door(door:Door):
     return door.state==DoorState.InitialSelection
+  return first(is_stick_door,doors)
 
+def choose_swap(doors):
   def is_swap_door(door:Door):
     return door.state==DoorState.Closed
-
-  def make_final_selection(self,doors)->Door:
-    raise Exception('Not implemented')
-
-class Swapper(Contestant):
-  def make_final_selection(self,doors)->Door:
-    return first(Contestant.is_swap_door,doors)
-
-class Sticker(Contestant):
-  def make_final_selection(self,doors)->Door:
-    return first(Contestant.is_stick_door,doors)
+  return first(is_swap_door,doors)
 
 
-
-class SwappingContestant(Contestant):
-  
-
-  def make_final_selection(self,doors)->Door:
-    def stick(door:Door):
-      return door.state==DoorState.InitialSelection
-    def swap(door:Door):
-      return door.state==DoorState.Closed
-    return first(stick,doors)
+def pick_door_to_reveal(doors):
+  def is_candidate(door:Door):
+    return door.state!=DoorState.InitialSelection and door.prize.value==0
+  return first(is_candidate,doors)
 
 
-@dataclass
-class Game:
-  host:Host
-  contestant:Contestant
-  doors:Tuple[Door,Door,Door]
+def play(chooser)->int:
+  doors = Doors.build_random()
 
-  def play(self)->int:
-    selection = self.contestant.make_initial_selection(self.doors)
-    selection.state=DoorState.InitialSelection
+  selection = random.choice(doors)
+  selection.state=DoorState.InitialSelection
 
-    revealed_goat = self.host.reveal_goat(self.doors)
-    revealed_goat.state=DoorState.Open
+  revealed_goat = pick_door_to_reveal(doors)
+  revealed_goat.state=DoorState.Open
 
-    final_selection = self.contestant.make_final_selection(self.doors)
-    print(f'Won a {final_selection.prize.name}!')
-    return final_selection.prize.value
+  final_selection = chooser(doors)
+  print(f'Won a {final_selection.prize.name}!')
+  return final_selection.prize.value
 
 
 total_winnings=0
 games_to_play=100
 
-host = Host('Monte')
-contestant = Sticker('Joe')
 
 for i in range(games_to_play):
-  doors = Doors.build_random()
-  game = Game(host,contestant,doors)
-  total_winnings += game.play()
+  
+  total_winnings += play(choose_stick)
 
 print(f'\n\nPer-game winnings: {total_winnings/games_to_play}')
 
