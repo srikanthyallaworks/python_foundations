b6d9b33be08daf633510d8c3630fd60f5f882319
bathcat <you@example.com>
2020-12-07 14:09:45 -0700

    .

--- labs/src/Q.0.MonteHall/complete/main.py
+++ labs/src/Q.0.MonteHall/complete/main.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from enum import Enum
 import random
 
-
 @dataclass(frozen=True)
 class Prize:
   name:str 
@@ -17,6 +16,13 @@ class DoorState(Enum):
 class Door:
   prize:Prize 
   state=DoorState.Closed
+  def __str__(self):
+    if self.state==DoorState.Open:
+      return self.prize.name
+    message = 'Closed'
+    if self.state==DoorState.InitialSelection:
+      message += ' *'
+    return message
 
 class Game:
   def __init__(self):
@@ -28,16 +34,11 @@ class Game:
     random.shuffle(prizes)
     self._doors= [Door(prize) for prize in prizes]
 
-  def print_state(self):
-    for i,d in enumerate(self._doors):
-      msg = f'Door #{i+1}: '
-      if(d.state==DoorState.Open):
-        msg += d.prize.name
-      elif d.state==DoorState.InitialSelection:
-        msg+= 'Closed *'
-      else:
-        msg+= 'Closed'
-      print(msg)
+  def __str__(self):
+    msg=''
+    for i,door in enumerate(self._doors):
+      msg+=f'Door #{i+1}: {door}\n'
+    return msg
 
   def reveal_goat(self):
     for d in self._doors:
@@ -54,25 +55,26 @@ class Game:
 
 
 
-print('\nLet us play!')
+def play_interactive():
+  game = Game()
+  print(game)
+  print('Pick a door [1,2,3]')
+  initial_door_no = int(input('? '))
+  game.select_initial(initial_door_no-1)
 
-game = Game()
-game.print_state()
-print('pick a door [1,2,3]')
-initial_door_no = int(input('? '))
-game.select_initial(initial_door_no-1)
+  print('\nNow, monty will reveal a goat:')
+  game.reveal_goat()
+  print(game)
 
-print('\nNow, monty will reveal a goat:')
-game.reveal_goat()
-game.print_state()
-
-print('\nMake your final selection [1,2,3]')
-final_door_no = int(input('? '))
-
-prize = game.select_final(final_door_no-1)
-
-print(f"You've won a {prize.name} worth ${prize.value}!!")
+  print('\nMake your final selection [1,2,3]')
+  final_door_no = int(input('? '))
+  prize = game.select_final(final_door_no-1)
+  print(f"You've won a {prize.name} worth ${prize.value}!!")
 
 
+def main():
+  print("Let's play a game!")
+  play_interactive()
 
-
+if __name__ == '__main__':
+  main()
