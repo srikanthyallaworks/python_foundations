e1af948a72c8b547f0e327481001d36bd0cf7bb6
bathcat <eric.mcmullen@gmail.cam>
2020-12-18 21:47:46 +0000

    Fixing hardware compatibility.

--- labs/src/A.1.HelloSetup/complete/heart.py
+++ labs/src/A.1.HelloSetup/complete/heart.py
@@ -5,9 +5,7 @@ import numpy
 # Adapted from:
 # https://learn.pimoroni.com/tutorial/tanya/be-still-my-beating-heart
 
-
 unicornhathd.brightness(1)
-unicornhathd.set_rotation(90)
 
 heart = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
          [0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0],
@@ -35,18 +33,27 @@ def numbers():
     yield from [11-n for n in range(1,11)]
     yield from range(1,10)
 
-while True:
-  for i in numbers():
-    for y in range(16):
-      for x in range(16):
-        h = 0.0
-        s = 1.0
-        v = heart[x,y] / float(i)
-        rgb = colorsys.hsv_to_rgb(h, s, v)
-        r = int(rgb[0]*255.0)
-        g = int(rgb[1]*255.0)
-        b = int(rgb[2]*255.0)
-        unicornhathd.set_pixel(x, y, r, g, b)
-    unicornhathd.show()
-    time.sleep(0.0005)
-  time.sleep(0.8)
+
+def beat():
+  while True:
+    for i in numbers():
+      for y in range(16):
+        for x in range(16):
+          h = 0.0
+          s = 1.0
+          v = heart[x,y] / float(i)
+          rgb = colorsys.hsv_to_rgb(h, s, v)
+          r = int(rgb[0]*255.0)
+          g = int(rgb[1]*255.0)
+          b = int(rgb[2]*255.0)
+          unicornhathd.set_pixel(x, y, r, g, b)
+      unicornhathd.show()
+      time.sleep(0.0005)
+    time.sleep(0.8)
+
+
+try:
+  beat()
+except KeyboardInterrupt:
+    unicornhathd.clear()
+    unicornhathd.off()
--- labs/src/A.1.HelloSetup/complete/mood.py
+++ labs/src/A.1.HelloSetup/complete/mood.py
@@ -14,19 +14,28 @@ def nudge(val,factor):
   nudged= val + random.gauss(0,factor)
   return pin(nudged)
 
-column = random.randrange(16)
-row = random.randrange(16)
-(r,g,b)=unicornhathd.get_pixel(row,column)
-(h,s,v) = colorsys.rgb_to_hsv(r/255,g/255,b/255)
+def random_walk():
 
-while True:
-  column = (column + random.randrange(-1,2))%16
-  row = (row + random.randrange(-1,2))%16
-  h = nudge(h,.005)
-  s = max(.85, nudge(s,.005))
-  v = max(.85,nudge(v,.01))
-  unicornhathd.set_pixel_hsv(row,column,h,s,v)
-  if random.random()>.9:
-    unicornhathd.show()
-    time.sleep(.1)
+  column = random.randrange(16)
+  row = random.randrange(16)
+  (r,g,b)=unicornhathd.get_pixel(row,column)
+  (h,s,v) = colorsys.rgb_to_hsv(r/255,g/255,b/255)
+
+  while True:
+    column = (column + random.randrange(-1,2))%16
+    row = (row + random.randrange(-1,2))%16
+    h = nudge(h,.005)
+    s = max(.85, nudge(s,.005))
+    v = max(.85,nudge(v,.01))
+    unicornhathd.set_pixel_hsv(row,column,h,s,v)
+    if random.random()>.9:
+      unicornhathd.show()
+      time.sleep(.1)
+
+try:
+  random_walk()
+except KeyboardInterrupt:
+    unicornhathd.clear()
+    unicornhathd.off()
+
 
