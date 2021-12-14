2cdf472942669a2b9e83cd8d87ad21257bea72ec
bathcat <you@example.com>
2021-12-14 10:13:26 -0700

    .

--- src/N.1.RESTfulFortune/startingpoint/main.py
+++ src/N.1.RESTfulFortune/startingpoint/main.py
@@ -4,7 +4,7 @@ cat_uri = 'https://cat-fact.herokuapp.com/facts/random'
 kanye_uri = 'http://api.kanye.rest'
 
 def get_fortune()->str:
-  """Gets a random quotation/fortune/cat fact from a public RESTful web service.      
+  """Gets a random quotation (or cat fact) from a public RESTful web service.      
   """
   return 'You will meet a mysterious stranger.'
 
