ccbca7fe8f1b249ddae6a2c7e28b49698bc389c2
bathcat <you@example.com>
2021-12-15 06:36:24 -0700

    Fixing linter complaint.

--- src/S.0.CLIWeather/startingpoint/main.py
+++ src/S.0.CLIWeather/startingpoint/main.py
@@ -4,12 +4,15 @@ import requests
 
 class Uris:
 
+  @staticmethod
   def for_geolocation():
     return 'http://www.geoplugin.net/json.gp'
 
+  @staticmethod
   def for_gridlocation(latitude,longitude):
     return f'https://api.weather.gov/points/{latitude},{longitude}'
 
+  @staticmethod
   def for_forecast(office,x,y):
     return f'https://api.weather.gov/gridpoints/{office}/{x},{y}/forecast'
 
