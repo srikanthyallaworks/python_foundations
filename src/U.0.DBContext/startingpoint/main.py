4efd7ff5e21d97032c1375b296cddf049041fffd
bathcat <you@example.com>
2021-12-15 09:19:58 -0700

    Incremental improvement.

--- src/U.0.DBContext/startingpoint/main.py
+++ src/U.0.DBContext/startingpoint/main.py
@@ -1,9 +1,13 @@
 from user_importer import UserImporter
-from users import users
+from user_service import UserService
 
 connection_string = 'Server=thor01;Database=Contoso;User Id=app;Password=5uper5ecret;'
+uri = 'https://example.com/users'
 
 def main():
+    service = UserService(uri)
+    users = service.get_users()
+
     importer = UserImporter(connection_string)
     importer.import_users(users)
 
