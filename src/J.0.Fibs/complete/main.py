bab300cda5fb794211b0e566b82696e5589495c6
bathcat <you@example.com>
2021-12-16 08:42:56 -0700

    Fixing linter complaints.

--- src/J.0.Fibs/complete/main.py
+++ src/J.0.Fibs/complete/main.py
@@ -1,53 +1,53 @@
-def is_even(n):
-  """Returns a value indicating whether the 
-  specified value is even
+def is_even(value):
+    """Returns a value indicating whether the 
+    specified value is even
 
-  Args:
-      n (int): some number
+    Args:
+        n (int): some number
 
-  Returns:
-      boolean: True means even; False means odd
-  """
-  return n%2==0
+    Returns:
+        boolean: True means even; False means odd
+    """
+    return value%2==0
 
-def get_fib_sequence(max):
-  """Gets a list containing the terms of the fibonacci
-  sequence up to and possibly including the specified max value.
+def get_fib_sequence(limit):
+    """Gets a list containing the terms of the fibonacci
+    sequence up to and possibly including the specified max value.
 
-  Args:
-      max (int): Maximum size of term in the generated
-        sequence
+    Args:
+        max (int): Maximum size of term in the generated
+          sequence
 
-  Returns:
-      list[int]: Fibonacci sequence
-  """
-  previous = 1
-  current = 2
-  terms = [previous]
-  while current<=max:
-    terms.append(current)
-    next = current + previous
-    previous = current
-    current = next
-  return terms
+    Returns:
+        list[int]: Fibonacci sequence
+    """
+    previous = 1
+    current = 2
+    terms = [previous]
+    while current<=limit:
+        terms.append(current)
+        next_term = current + previous
+        previous = current
+        current = next_term
+    return terms
 
 
 def get_euler02_solution():
-  """Gets the solution.
+    """Gets the solution.
 
-  Returns:
-      int: Sum of even terms of the fibonacci 
-      sequence below 4 million
-  """
-  limit = 4e6
-  sum = 0
-  for n in get_fib_sequence(limit):
-    if is_even(n):
-      sum += n
-  return sum
+    Returns:
+        int: Sum of even terms of the fibonacci 
+        sequence below 4 million
+    """
+    limit = 4e6
+    total = 0
+    for term in get_fib_sequence(limit):
+        if is_even(term):
+            total += term
+    return total
 
 def main():
-  print(f'Here is your answer: {get_euler02_solution()}')
+    print(f'Here is your answer: {get_euler02_solution()}')
 
 if __name__ == "__main__":
     main()
