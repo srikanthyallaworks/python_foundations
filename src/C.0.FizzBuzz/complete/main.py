5470d32ef357e36082efffa64bea32f3ef3f38e8
bathcat <you@example.com>
2021-12-22 11:27:52 -0700

    Adding poetry to manage environment and dependencies.

--- src/C.0.FizzBuzz/complete/main.py
+++ src/C.0.FizzBuzz/complete/main.py
@@ -10,25 +10,23 @@ def toText(n):
 
     is_divisible_by_3 = n % 3 == 0
     is_divisible_by_5 = n % 5 == 0
-    
+
     if is_divisible_by_3 and is_divisible_by_5:
         return "fizzbuzz"
-    
+
     if is_divisible_by_3:
         return "fizz"
-    
+
     if is_divisible_by_5:
         return "buzz"
-        
+
     return str(n)
 
 
 def main():
-    for i in range(1,101):
+    for i in range(1, 101):
         print(toText(i))
 
 
 if __name__ == "__main__":
     main()
-
-
--- src/N.1.RESTfulFortune/complete/main.py
+++ src/N.1.RESTfulFortune/complete/main.py
@@ -1,40 +1,37 @@
 import requests
 
 
+def get_kanye_fortune() -> str:
+    """Hits a public API to get a random quotation from Kanye West
 
-def get_kanye_fortune()->str:
-  """Hits a public API to get a random quotation from Kanye West
-
-  Returns:
-      str: Kanye Quote. Warning: he gets salty
-  """
-  response = requests.get('http://api.kanye.rest')
-  json = response.json()
-  response.close()
-  return json['quote']
-
+    Returns:
+        str: Kanye Quote. Warning: he gets salty
+    """
+    response = requests.get('http://api.kanye.rest')
+    json = response.json()
+    response.close()
+    return json['quote']
 
 
+def get_cat_fortune() -> str:
+    """Hits a public API to get a random cat fact
 
-def get_cat_fortune()->str:
-  """Hits a public API to get a random cat fact
-
-  Returns:
-      str: Cat fact
-  """
-  response = requests.get('https://cat-fact.herokuapp.com/facts/random')
-  json = response.json()
-  response.close()
-  return json['text']
+    Returns:
+        str: Cat fact
+    """
+    response = requests.get('https://cat-fact.herokuapp.com/facts/random')
+    json = response.json()
+    response.close()
+    return json['text']
 
 
 get_fortune = get_kanye_fortune
 
 
 def main():
-  message = get_fortune()
-  print('\nYour fortune is:')
-  print(f'   {message}\n\n')
+    message = get_fortune()
+    print('\nYour fortune is:')
+    print(f'   {message}\n\n')
 
 
 if __name__ == "__main__":
