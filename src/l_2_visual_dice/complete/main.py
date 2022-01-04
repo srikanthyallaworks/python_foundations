b7dffc466227cb9b1b223c82df406d5be118cd05
bathcat <you@example.com>
2022-01-04 11:56:28 -0700

    Adding placeholder for flask-based web app.

--- src/l_2_visual_dice/complete/main.py
+++ src/l_2_visual_dice/complete/main.py
@@ -1,8 +1,33 @@
 from dice import Die, Cup
+from typing import Final
+import flask
+
+app = flask.Flask(__name__)    # Create an instance of the class for our use
+
+
+ts: Final = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>xyzzzzzzzzzzzz</title>
+    </head>
+    <body>
+        <div class="body-content">
+            <h1>Hello World!</h1>
+        </div>
+    </body>
+</html>
+'''
+
+
+@app.route("/")
+def home():
+    return flask.render_template_string(ts)
 
 
 def main():
-    print('xyz')
+    app.run('0.0.0.0', 5000)
 
 
 if __name__ == "__main__":
