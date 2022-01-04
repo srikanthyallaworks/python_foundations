6e6515c39a1c0fb2eac99e7230953a40d6ee9b46
bathcat <you@example.com>
2022-01-04 08:37:48 -0700

    .

---
+++ src/l_2_visual_dice/complete/main.py
@@ -0,0 +1,9 @@
+from dice import Die, Cup
+
+
+def main():
+    print('xyz')
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/m_0_linked_list/complete/node.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from functools import reduce
+from typing import Generic, TypeVar, Optional, Generator
+from typing_extensions import Self
+
+T = TypeVar('T')
+
+
+@dataclass(frozen=True)
+class Node(Generic[T]):
+    """Node in a linked list
+
+    Args:
+        Generic ([type]): Type of thing to hold.
+    """
+    value: T
+    tail: Optional[Self]
+
+@dataclass()
+class LinkedList(Generic[T]):
+    head: Optional[Node[T]]
+
+    def __get_items(self) -> Generator[T, None, None]:
+        hed = self.head
+        while hed != None:
+            yield hed.value
+            hed = hed.tail
+
+    def __len__(self) -> int:
+        return reduce(lambda count, _: count + 1, self.__get_items(), 0)
