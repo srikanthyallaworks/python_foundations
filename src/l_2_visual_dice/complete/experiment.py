2291d24c4aad305fe3217c45eace40a9810fdf9c
bathcat <you@example.com>
2022-01-05 08:56:32 -0700

    .

--- src/l_2_visual_dice/complete/experiment.py
+++ src/l_2_visual_dice/complete/experiment.py
@@ -9,23 +9,28 @@ class DataPoint:
     sum: int
     roll_count: int
 
+
 Results = Tuple[DataPoint, ...]
+
 
 @dataclass(frozen=True)
 class Parameters:
-    dice_count:int
+    dice_count: int
     roll_count: int
+
 
 @dataclass(frozen=True)
 class Experiment:
-    name:str
-    parameters:Parameters
+    name: str
+    parameters: Parameters
     results: Results
 
 
-def run_experiment(name:str,parameters:Parameters=Parameters(10,5000)) -> Experiment:
+def run_experiment(
+    name: str, parameters: Parameters = Parameters(10, 5000)
+) -> Experiment:
     cup = Cup(parameters.dice_count)
     raw_data = [cup.roll() for _ in range(parameters.roll_count)]
     data_points = [DataPoint(s, rc) for s, rc in Counter(raw_data).items()]
     sorted_points = sorted(data_points, key=lambda dp: dp.sum)
-    return Experiment(name,parameters,tuple(sorted_points))
+    return Experiment(name, parameters, tuple(sorted_points))
--- src/l_2_visual_dice/complete/main.py
+++ src/l_2_visual_dice/complete/main.py
@@ -2,7 +2,9 @@ from web import run
 
 
 def main():
+    print("\n\n***************************")
     run()
+    print("\n***************************\n\n")
 
 
 if __name__ == "__main__":
